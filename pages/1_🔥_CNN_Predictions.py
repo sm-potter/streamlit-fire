dfec69929351d4a5e00d0de97a2ceaba3f5e2463
spotter <stefano.m.potter@gmail.com>
2022-12-27 11:58:00 -0500

    trying ee authenticate

--- pages/1_🔥_CNN_Predictions.py
+++ pages/1_🔥_CNN_Predictions.py
@@ -2,6 +2,8 @@ import streamlit as st
 import leafmap.foliumap as leafmap
 import geemap.foliumap as geemap
 import ee
+import warnings
+
 
 # ee.Authenticate()
 # ee.Initialize()
@@ -11,6 +13,49 @@ import ee
 import geemap.colormaps as cm
 
 st.set_page_config(layout="wide")
+
+warnings.filterwarnings("ignore")
+
+
+@st.cache(persist=True)
+def ee_authenticate(token_name="EARTHENGINE_TOKEN"):
+    geemap.ee_initialize(token_name=token_name)
+
+ee_authenticate(token_name="EARTHENGINE_TOKEN")
+
+
+# year = st.selectbox(
+#                     "Select Fire Year:",
+#                     [
+#                         "1985",
+#                         "1986",
+#                         "1987",
+#                         "1988",
+#                         "1999",
+#                         "2000",
+#                         "2001",
+#                         "2002",
+#                         "2003",
+#                         "2004",
+#                         "2005",
+#                         "2006",
+#                         "2007",
+#                         "2008",
+#                         "2009",
+#                         "2010",
+#                         "2011",
+#                         "2012",
+#                         "2013",
+#                         "2014",
+#                         "2015",
+#                         "2016",
+#                         "2017",
+#                         "2018",
+#                         "2019",
+#                         "2020"
+#                     ],
+#                     index=9,
+#                 )
 
 
 st.sidebar.title("About")
@@ -36,19 +81,29 @@ st.title("Alaska CNN Predictions")
 Map = geemap.Map()
 
 #read in the predictions and lfdb ground truth
-dnbr = ee.ImageCollection("projects/gee-serdp-upload/assets/mtbs_predictions").select('b7')
+in_collection = ee.ImageCollection("projects/gee-serdp-upload/assets/mtbs_predictions")
+
+red = in_collection = in_collection.select('b1')
+green = in_collection = in_collection.select('b2')
+blue = in_collection = in_collection.select('b3')
+nir = in_collection = in_collection.select('b4')
+swir1 = in_collection = in_collection.select('b5')
+swir2 = in_collection = in_collection.select('b6')
+dnbr = in_collection = in_collection.select('b7')
+ndii  = in_collection = in_collection.select('b8')
+ndvi = in_collection = in_collection.select('b9')
 preds = ee.ImageCollection("projects/gee-serdp-upload/assets/mtbs_predictions").select('b10')
 preds_thresh = ee.ImageCollection("projects/gee-serdp-upload/assets/mtbs_predictions").select('b11')
 
-lfdb = ee.FeatureCollection("users/spotter/fire_cnn/raw/ak_mtbs_1985") #ak fire polygons)
+lfdb = ee.FeatureCollection("users/spotter/fire_cnn/raw/ak_mtbs_1985")
 
-palette = cm.palettes.RdBu_r
+dnbr_palette = cm.palettes.RdBu_r
 
 
+Map.setCenter(-151.29, 65.11, 4)
 Map.addLayer(preds, {'min': 0, 'max': 1}, 'Predictions')
 Map.addLayer(preds_thresh, {'min': 0, 'max': 1}, 'Predictions Threshold')
-Map.addLayer(dnbr, {min:0, max: 1, 'palette' : palette}, 'dNBR')
-
+Map.addLayer(dnbr, {min:0, max: 1, 'palette' : dnbr_palette}, 'dNBR')
 Map.addLayer(lfdb, {}, 'Ground Truth MTBS Polygons')
 
 width = 950
